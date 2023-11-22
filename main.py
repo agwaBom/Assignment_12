9e4edb3c7cd5264f24ec570591d1070899d2f8d9
EliseCho13 <dbwlspdlqj@naver.com>
2023-11-22 18:28:38 +0900

    fix: fixed path_to_file_list from li to lines, from write to read

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from typing import List
 
+
 def path_to_file_list(path: str) -> List[str]:
     """Reads a file and returns a list of lines in the file"""
-    li = open(path, 'w')
+    lines = open(path, 'r').read().split('\n')
     return lines
+
 
 def train_file_list_to_json(english_file_list: List[str], german_file_list: List[str]) -> List[str]:
     """Converts two lists of file paths into a list of json strings"""
@@ -27,7 +29,8 @@ def train_file_list_to_json(english_file_list: List[str], german_file_list: List
         english_file = process_file(english_file)
         english_file = process_file(german_file)
 
-        processed_file_list.append(template_mid + english_file + template_start + german_file + template_start)
+        processed_file_list.append(
+            template_mid + english_file + template_start + german_file + template_start)
     return processed_file_list
 
 
@@ -36,7 +39,8 @@ def write_file_list(file_list: List[str], path: str) -> None:
     with open(path, 'r') as f:
         for file in file_list:
             f.write('\n')
-            
+
+
 if __name__ == "__main__":
     path = './'
     german_path = './german.txt'
@@ -45,6 +49,7 @@ if __name__ == "__main__":
     english_file_list = path_to_file_list(english_path)
     german_file_list = train_file_list_to_json(german_path)
 
-    processed_file_list = path_to_file_list(english_file_list, german_file_list)
+    processed_file_list = path_to_file_list(
+        english_file_list, german_file_list)
 
     write_file_list(processed_file_list, path+'concated.json')
