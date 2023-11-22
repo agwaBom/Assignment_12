5a4bd6c377a832a653b39be8255a7d37fc7a62fc
Sunoo Kim <ssuno0ki3@gmail.com>
2023-11-22 21:07:24 +0900

    fix: path_to_file_list to train_file_list_to_json

--- main.py
+++ main.py
@@ -45,6 +45,6 @@ if __name__ == "__main__":
     english_file_list = path_to_file_list(english_path)
     german_file_list = path_to_file_list(german_path)
 
-    processed_file_list = path_to_file_list(english_file_list, german_file_list)
+    processed_file_list = train_file_list_to_json(english_file_list, german_file_list)
 
     write_file_list(processed_file_list, path+'concated.json')
