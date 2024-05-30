53d0454b2edb89177665fb5186cf9fbd91ad7e69
gohbc03 <gohbc@g.skku.edu>
2024-05-30 09:37:43 +0000

    the correction has been made.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from typing import List
 def path_to_file_list(path: str) -> List[str]:
     """Reads a file and returns a list of lines in the file"""
     li = open(path, 'w')
-    return lines
+    return li
 
 def train_file_list_to_json(english_file_list: List[str], german_file_list: List[str]) -> List[str]:
     """Converts two lists of file paths into a list of json strings"""
@@ -27,7 +27,7 @@ def train_file_list_to_json(english_file_list: List[str], german_file_list: List
         english_file = process_file(english_file)
         english_file = process_file(german_file)
 
-        processed_file_list.append(template_mid + english_file + template_start + german_file + template_start)
+        processed_file_list.append(template_start + english_file + template_mid + german_file + template_end)
     return processed_file_list
 
 
