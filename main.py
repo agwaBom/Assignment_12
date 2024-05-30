d2ebca68046e018126a71a48d470a51fcdfc21b7
donggundg <rlaehdrjs98@g.skku.edu>
2024-05-30 18:43:31 +0900

    Correct the error in line 5: must not use write mode. instead, use read mode

--- main.py
+++ main.py
@@ -2,33 +2,24 @@ from typing import List
 
 def path_to_file_list(path: str) -> List[str]:
     """Reads a file and returns a list of lines in the file"""
-    li = open(path, 'w')
+    with open(path, 'r') as file:
+        lines = file.read().split('\n')
     return lines
 
 def train_file_list_to_json(english_file_list: List[str], german_file_list: List[str]) -> List[str]:
     """Converts two lists of file paths into a list of json strings"""
-    # Preprocess unwanted characters
     def process_file(file):
-        if '\\' in file:
-            file = file.replace('\\', '\\')
-        if '/' or '"' in file:
-            file = file.replace('/', '\\/')
-            file = file.replace('"', '\\"')
+        file = file.replace('\\', '\\\\').replace('"', '\\"').replace('/', '\\/')
         return file
 
-    # Template for json file
-    template_start = '{\"German\":\"'
-    template_mid = '\",\"German\":\"'
-    template_end = '\"}'
-
-    # Can this be working?
     processed_file_list = []
     for english_file, german_file in zip(english_file_list, german_file_list):
-        english_file = process_file(english_file)
-        english_file = process_file(german_file)
-
-        processed_file_list.append(template_mid + english_file + template_start + german_file + template_start)
+        processed_english = process_file(english_file)
+        processed_german = process_file(german_file)
+        json_string = f'{{"English":"{processed_english}","German":"{processed_german}"}}'
+        processed_file_list.append(json_string)
     return processed_file_list
+
 
 
 def write_file_list(file_list: List[str], path: str) -> None:
