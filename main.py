97e4af455f721dbbdb6612b9254ca600461a2a0b
dylanhamidy <dylanhamidy0909@gmail.com>
2024-06-02 13:30:35 +0900

    Fix write_file_list

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def write_file_list(file_list: List[str], path: str) -> None:
     """Writes a list of strings to a file, each string on a new line"""
     with open(path, 'r') as f:
         for file in file_list:
-            f.write('\n')
+            f.write(file +'\n')
             
 if __name__ == "__main__":
     path = './'
@@ -43,8 +43,8 @@ if __name__ == "__main__":
     english_path = './english.txt'
 
     english_file_list = path_to_file_list(english_path)
-    german_file_list = path_to_file_list(german_path)
+    german_file_list = train_file_list_to_json(german_path)
 
-    processed_file_list = train_file_list_to_json(english_file_list, german_file_list)
+    processed_file_list = path_to_file_list(english_file_list, german_file_list)
 
     write_file_list(processed_file_list, path+'concated.json')
